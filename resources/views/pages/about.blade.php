08b592cfb3fd2d262fd48d50a116cd20ef4fe712
Dhruveen <dnp49@njit.edu>
2018-10-20 19:45:10 -0400

    Edit: Edited three files from pages directory

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+
+@section('content')
+    <h1>This is about page</h1>
+    <p class="lead">Welocme</p>
+@endsection
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.default')
+
+@section('content')
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+        <div class="container">
+            <h1 class="display-3">Hello, world!</h1>
+            <p>Welocme to Laravel</p>
+            <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+        </div>
+    </div>
+
+    <div class="container">
+        <!-- Example row of columns -->
+        <div class="row">
+            <div class="col-md-4">
+                <h2>Heading</h2>
+                <p>Description </p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            </div>
+            <div class="col-md-4">
+                <h2>Heading</h2>
+                <p>Description </p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            </div>
+            <div class="col-md-4">
+                <h2>Heading</h2>
+                <p>Description</p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            </div>
+        </div>
+        <hr>
+    </div> <!-- /container -->
+@endsection
